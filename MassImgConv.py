9ad482654a29732a95e0992df603e3035e0eaacb
EuroNutellaMan <lori23101@gmail.com>
2023-05-15 18:46:53 +0200

    Add files to Git LFS

---
+++ MassImgConv.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3bde85659855b78e072e3db0b1a0ca421445aaca5e7b296df907d75bf49282f
+size 2732
---
+++ MassImgConv.spec
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d1f3df9315455c5e143bed08d019f14871ce28e831a332bb5b3c48ccfecdea0
+size 866
